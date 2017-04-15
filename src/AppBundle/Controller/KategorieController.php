e56a3d53d22187615e27e06da91e8d502dd235c6
Anna Zaremba <aniazarem@gmail.com>
2017-04-15 17:26:23 +0200

    pobieranie z bazy danych

---
+++ src/AppBundle/Controller/KategorieController.php
@@ -0,0 +1,28 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Repository\Doctrine\KategoriaRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class KategorieController
+ * @package AppBundle\Controller
+ * @Route("/kategorie")
+ */
+class KategorieController extends Controller
+{
+    /**
+     * @Route("/{id}", name="kategoriaid", requirements={"id": "\d+"})
+     * @Template()
+     */
+    public function findAction($id)
+    {
+        return [
+            'kategoria' => (new KategoriaRepository($this->getDoctrine()->getManager()))->getOneById($id),
+            'wpisy' => (new KategoriaRepository($this->getDoctrine()->getManager()))->getOneById($id)->getWpisy(),
+            'kategorie' => (new KategoriaRepository($this->getDoctrine()->getManager()))->getAllOrderByName(),
+        ];
+    }
+}

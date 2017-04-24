8266dcdfd2a0b32497b8e901df7dc2a2feb5bf54
Anna Zaremba <aniazarem@gmail.com>
2017-04-24 12:32:46 +0200

    kategorie

--- src/AppBundle/Controller/KategorieController.php
+++ src/AppBundle/Controller/KategorieController.php
@@ -1,64 +1,47 @@
 <?php
+
 namespace AppBundle\Controller;
 
 use AppBundle\Repository\Doctrine\KategoriaRepository;
+use AppBundle\Repository\Doctrine\WpisRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-///**
-// * Class KategorieController
-// * @package AppBundle\Controller
-// * @Route("/kategorie")
-// * @Route(service="app.kategorie_controller")
-// */
-//class KategorieController extends Controller
-//{
-//    /** @var KategoriaRepository */
-//    private $kategoriaRepository;
-//
-//    /**
-//     * @param KategoriaRepository $kategoriaRepository
-//     */
-//    public function __construct(KategoriaRepository $kategoriaRepository)
-//    {
-//        $this->kategoriaRepository = $kategoriaRepository;
-//    }
-//
-//    /**
-//     * @Route("/{id}", name="kategoriaid", requirements={"id": "\d+"})
-//     * @Template()
-//     * @param $id
-//     * @return array
-//     */
-//    public function findAction($id)
-//    {
-//        return [
-//            'kategoria' => $this->kategoriaRepository->getOneById($id),
-//            'wpisy' => $this->kategoriaRepository->getOneById($id)->getWpisy(),
-//            'kategorie' => $this->kategoriaRepository->getAllOrderByName(),
-//        ];
-//    }
-//}
-
-
 /**
- * Class KategorieController
- * @package AppBundle\Controller
- * @Route("/kategorie")
+ * @Route(service="app.kategorie_controller")
  */
 class KategorieController extends Controller
 {
+    /** @var KategoriaRepository */
+    private $kategoriaRepository;
+
+    /** @var WpisRepository */
+    private $wpisRepository;
+
     /**
-     * @Route("/{id}", name="kategoriaid", requirements={"id": "\d+"})
-     * @Template()
+     * @param KategoriaRepository $kategoriaRepository
+     * @param WpisRepository $wpisRepository
      */
-    public function findAction($id)
+    public function __construct(KategoriaRepository $kategoriaRepository, WpisRepository $wpisRepository)
+    {
+        $this->kategoriaRepository = $kategoriaRepository;
+        $this->wpisRepository = $wpisRepository;
+    }
+
+    /**
+     * @Route("/kategoria{id}", name="kategoriaid", requirements={"id": "\d+"})
+     * @Template()
+     *
+     * @param int $id
+     * @return array
+     */
+    public function znajdzAction($id)
     {
         return [
-            'kategoria' => (new KategoriaRepository($this->getDoctrine()->getManager()))->getOneById($id),
-            'wpisy' => (new KategoriaRepository($this->getDoctrine()->getManager()))->getOneById($id)->getWpisy(),
-            'kategorie' => (new KategoriaRepository($this->getDoctrine()->getManager()))->getAllOrderByName(),
+            'kategoria' =>  $this->kategoriaRepository->getOneById($id),
+            'wpisy' => $this->kategoriaRepository->getOneById($id)->getWpisy(),
+            'kategorie' =>  $this->kategoriaRepository->getAllOrderByName(),
         ];
     }
 }

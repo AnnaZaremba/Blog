bfd4364d14f7112eea8291b994a32ef0b926c62a
Anna Zaremba <aniazarem@gmail.com>
2017-05-01 14:37:37 +0200

    poprawki

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use AppBundle\Repository\Doctrine\KategoriaRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class SecurityController
